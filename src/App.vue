90bbdd7a68427085cae6e822231a9a9c3433608b
Alexandr <140608639+Ryazan8@users.noreply.github.com>
2023-11-01 21:29:09 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="app">
+    <post-form
+        @create="createPost"
+    />
+    <post-list
+        :posts="posts"
+        @remove="removePost"
+    />
+  </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm.vue";
+import PostList from "@/components/PostList.vue";
+export default {
+  components: {
+    PostForm,PostList
+  },
+
+  data () {
+    return{
+      posts:[
+        {id:1, title: 'Название', body: 'Контент поста'},
+        {id:2, title: 'Название 2', body: 'Контент поста 2'}
+      ],
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post)
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p !== post.id)
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.app{
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
+
+</style>
---
+++ src/components/PostItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="post ">
+    <div>
+      <div><strong>Название:</strong>{{ post.title }}</div>
+      <div><strong>Описание:</strong>{{ post.body }}</div>
+    </div>
+    <div class="post__btns">
+      <my-button @remove="$emit('remove', post)">
+        Удалить
+      </my-button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post{
+  padding: 15px;
+  border: 2px dotted #cf5353;
+  margin: 15px auto;
+  max-width: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
